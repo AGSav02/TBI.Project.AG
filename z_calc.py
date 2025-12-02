b3658f5775b1f25d5b32b89d8319e64eb64d7ed6
AGSav02 <a12418488@unet.univie.ac.at>
2025-12-02 10:05:34 +0100

    updates

--- z_calc.py
+++ z_calc.py
@@ -1,36 +1,45 @@
 import argparse
 import random
+import numpy
+import RNA
+import csv 
+import os
 
 def sequence_shuffle(sequence, n):
     sequence = sequence.upper()
-    shuffled_sequences = []
-    for j in range(n):
+    sequences = [sequence]
+    for _ in range(n):
         shuffled = list(sequence)
         random.shuffle(shuffled)
-        shuffled_seq = ''.join(shuffled)
-    return shuffled_sequences
+        shuffled = "".join(shuffled)
+        sequences.append(shuffled)
+    return sequences
 
 def cal_z(sequence, n):
+    sequences = sequence_shuffle(sequence, n)
+
     mfes = []
-    shuffled_sequences = sequence_shuffle(sequence, n)
-    for seq in shuffled_sequences:
+    for seq in sequences:
         fc = RNA.fold_compound(seq)
         structure, mfe_value = fc.mfe()
         mfes.append(mfe_value)
 
     original_mfe = mfes[0]
-    shuffled_mfes = mfes
+    shuffled_mfes = mfes[1:]
+
     mean_shuf = numpy.mean(shuffled_mfes)
     std_shuf = numpy.std(shuffled_mfes)
     z_score = (original_mfe - mean_shuf) / std_shuf if std_shuf != 0 else 0
 
+    return original_mfe, sequences[1:], shuffled_mfes, z_score
+
 def main():
 
     parser = argparse.ArgumentParser(
-        description = "Shuffle 200nt sequence. Calculates the mfe of the original sequence and the its z-score."
+        description = "Shuffle RNA sequence, calculate MFEs and z-score."
     )
 
-    parser.add_argument("sequence", type=str, help="The 200nt RNA sequence.")
+    parser.add_argument("sequence", type=str, help="The RNA sequence.")
 
     parser.add_argument(
         "-n", "--number",
@@ -42,25 +51,39 @@ def main():
     parser.add_argument(
         "-o", "--output",
         type=str,
-        help = "Output file to save the original sequence, the shuffled sequences along with the mfes and the calculated z-score."
+        help = "TSV output file containing the sequences, MFEs and the z-score."
     )
 
+    parser.add_argument(
+        "-f", "--folder",
+        type=str,
+        help = "Makes a folder containing all the output files within the directory."
+    )
+    
     args = parser.parse_args()
 
-    shuffle_results = sequence_shuffle(args.sequence, args.number)
-    z_results = cal_z(args.sequence)
+    original_mfe, sequences, shuffled_mfes, z_score = cal_z(
+        args.sequence,
+        args.number
+    )
 
     if args.output:
-        with open(args.output, 'w', newline ='') as tsvfile:
-            writer = csv.writer(tsvfile, delimiter = '\t')
-            writer.writerow(["seq", 'mfe', 'z_score'])
-            writer.writerow([shuffled_sequences[0][1], original_mfe, z_score])
-            for seq, mfe in zip(sequences[1:], mfes[1:]):
+        if args.folder:
+            os.makedirs(args.folder, exist_ok=True)
+            output_path = os.path.join(args.folder, args.output)
+        else:
+            output_path = args.output
+        with open(output_path, 'w', newline='') as tsvfile:
+            writer = csv.writer(tsvfile, delimiter='\t')
+            writer.writerow(["seq", "mfe", "z_score"])
+            writer.writerow([args.sequence.upper(), original_mfe, z_score])
+            for seq, mfe in zip(sequences, shuffled_mfes):
                 writer.writerow([seq, mfe, ''])
-        print(f"Protein saved to {args.output}!")    
+        print(f"File saved to: {output_path}")
     else:
-        print("mfe/z-score:")
-        print(z_results)
+        print("Sequence:", args.sequence)
+        print("Original MFE:", original_mfe)
+        print("z-score:", z_score)
 
 if __name__ == "__main__":
     main()
